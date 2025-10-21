fccbfc3321ffbfa938e26bf3debcb1308f22986d
Nicholas Smith <nsmith196@student.cscc.edu>
2025-10-21 18:36:29 -0400

    Add project files.

---
+++ Demo/Revolution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Demo.Models
+{
+    public class Revolution
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Country { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+    }
+}
